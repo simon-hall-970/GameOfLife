bf4636ecf937d533a763d7cc45a8f4f3b2976c5c
Simon Hall <simon970@gmail.com>
2020-08-10 20:04:52 +1200

    fix indices are out of bounds function to check correct width boundaries

--- client/components/GameFunctions/indicesAreOutOfBounds.js
+++ client/components/GameFunctions/indicesAreOutOfBounds.js
@@ -1,7 +1,7 @@
 import isOutOfBounds from './isOutOfBounds'
 
 function indicesAreOutOfBounds (rowIndex, columnIndex, array) {
-    return (isOutOfBounds(rowIndex, array) || isOutOfBounds(columnIndex, array))
+    return (isOutOfBounds(rowIndex, array) || isOutOfBounds(columnIndex, array[0]))
 }
 
 export default indicesAreOutOfBounds
--- client/components/GameFunctions/nextBoard.js
+++ client/components/GameFunctions/nextBoard.js
@@ -2,30 +2,31 @@ import nextCellState from './nextCellState'
 import countAliveNeighbours from './countAliveNeighbours'
 
 function nextBoard(currentBoard) {
-    //create empty array for next generation i.e. next board
-    let nextGen = []
 
-    //loop through each row in the current board
+    let nextGen = []
+    console.log("currentBoard :",currentBoard)
+
     for(let i=0; i<currentBoard.length; i++) {
 
-        //create an empty array for the new row
+     
         let row=[]
 
-        //loop through each cell in the current row
+
         for(let j=0; j<currentBoard[i].length; j++) {
 
-            //get state and number of alive neighbours for current cell
+            //could be in countAliveNeighbours
             let neighCount = countAliveNeighbours(i,j,currentBoard)
             let cellState = currentBoard[i][j]
 
-            //determine next generation of current cell then push to row
+            //could be in nextCellState
             let nextCellGen = nextCellState(cellState, neighCount)
             row.push(nextCellGen)
         }
-        //after looping through each cell in a row push the row to the next board generation
+        
         nextGen.push(row)
     }
-    //after looping through all cells in all rows return final board
+
+    console.log("nextGen: ", nextGen)
      return nextGen
 }
 
