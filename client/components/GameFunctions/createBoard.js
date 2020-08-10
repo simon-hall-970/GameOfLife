dc28d96ab1686c29c376a61b7d35ecbec3ba2505
Simon Hall <simon970@gmail.com>
2020-08-10 13:56:26 +1200

    update createboard function to accept height and width

--- client/components/GameFunctions/createBoard.js
+++ client/components/GameFunctions/createBoard.js
@@ -1,12 +1,15 @@
-function createBoard (size) {
-    let board = []
+function createBoard (height, width=height) {
+    //if width argument is not supplied it defaults to height argument creating a square
+    
+    let board = [] //set empty array
 
-    for(let i = 0; i < size; i++) {
-        board.push(Array(size).fill(0))
+    //loop through to create rows depending on height argument
+    for(let i = 0; i < height; i++) {
+
+        //push array with width number of cells initially set to empty(dead) cells
+        board.push(Array(width).fill(0))
     }
     return board
 }
-
-
 
 module.exports = createBoard
